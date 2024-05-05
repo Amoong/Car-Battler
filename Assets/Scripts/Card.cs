2dc5f0f6abbdd4636d544f6b9eee13ac33a047c8
Amoong <moo1323@naver.com>
2024-05-05 15:33:31 +0900

    Add more info to CardScriptableObject

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,21 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
+    public CardScriptableObject cardSO;
+
     public int currentHealth;
     public int attackPower;
     public int manaCost;
 
-    public TMP_Text healthText, attackText, costText;
+    public TMP_Text healthText, attackText, costText, nameText, actionDescriptionText, loreText;
+
+    public Image characterArt, bgArt;
 
     // Start is called before the first frame update
     void Start()
     {
+        SetupCard();
+    }
+
+    public void SetupCard()
+    {
+        currentHealth = cardSO.currentHealth;
+        attackPower = cardSO.attackPower;
+        manaCost = cardSO.manaCost;
+
         healthText.text = currentHealth.ToString();
         attackText.text = attackPower.ToString();
         costText.text = manaCost.ToString();
+
+        nameText.text = cardSO.cardName;
+        actionDescriptionText.text = cardSO.actionDescription;
+        loreText.text = cardSO.cardLore;
+
+        characterArt.sprite = cardSO.characterSprite;
+        bgArt.sprite = cardSO.bgSprite;
     }
 
     // Update is called once per frame
