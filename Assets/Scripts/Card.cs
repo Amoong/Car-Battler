0f66e539e1c4126c86efc6e022df0ea703655852
Amoong <moo1323@naver.com>
2024-05-05 14:29:46 +0900

    Update card ability text

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Card : MonoBehaviour
 {
+    public int currentHealth;
+    public int attackPower;
+    public int manaCost;
+
+    public TMP_Text healthText, attackText, costText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        healthText.text = currentHealth.ToString();
+        attackText.text = attackPower.ToString();
+        costText.text = manaCost.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
