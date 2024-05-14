a0dda4ecdec5903cfa8a5e088b59962fe8265755
Amoong <moo1323@naver.com>
2024-05-14 21:31:53 +0900

    Make enemy place cards

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -14,6 +14,9 @@ public class EnemyController : MonoBehaviour
     public List<CardScriptableObject> deckToUse = new List<CardScriptableObject>();
     private List<CardScriptableObject> activeCards = new List<CardScriptableObject>();
 
+    public Card cardToSpawn;
+    public Transform cardSpawnPoint;
+
     void Start()
     {
         SetupDeck();
@@ -51,6 +54,34 @@ public class EnemyController : MonoBehaviour
         if (activeCards.Count == 0)
         {
             SetupDeck();
+        }
+
+        yield return new WaitForSeconds(.5f);
+
+        List<CardPlacePoint> cardPoints = new List<CardPlacePoint>();
+        cardPoints.AddRange(CardPointsController.instance.enemyCardPoints);
+
+        int randomPoint = Random.Range(0, cardPoints.Count);
+        CardPlacePoint selectedPoint = cardPoints[randomPoint];
+
+        // Find empty selectedPoint
+        while (selectedPoint.activeCard != null && cardPoints.Count > 0)
+        {
+            randomPoint = Random.Range(0, cardPoints.Count);
+            selectedPoint = cardPoints[randomPoint];
+            cardPoints.RemoveAt(randomPoint);
+        }
+
+        if (selectedPoint.activeCard == null)
+        {
+            Card newCard = Instantiate(cardToSpawn, cardSpawnPoint.position, cardSpawnPoint.rotation);
+            newCard.cardSO = activeCards[0];
+            activeCards.RemoveAt(0);
+            newCard.SetupCard();
+            newCard.MoveToPoint(selectedPoint.transform.position, selectedPoint.transform.rotation);
+
+            selectedPoint.activeCard = newCard;
+            newCard.assignedPlace = selectedPoint;
         }
 
         yield return new WaitForSeconds(.5f);
